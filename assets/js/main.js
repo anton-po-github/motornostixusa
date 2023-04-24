dd3a9ed93b57b7f7fc7af66510a0395dacc74a42
AntonPo <myworkinggemail@gmail.com>
2023-04-24 12:55:42 +0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,7 @@
       }
     };
     window.addEventListener('load', headerScrolled);
-    onscroll(document, headerScrolled); 
+    onscroll(document, headerScrolled);
   }
 
   /**
@@ -346,13 +346,14 @@
   });
 })();
 
-
 const flickerDownloadNew = () => {
   element = document.getElementById('download-new');
 
   if (element) {
-    element.animate([{opacity:0.5},{opacity:1}],{duration:2000,iterations:30})
+    element.animate([{ opacity: 0.5 }, { opacity: 1 }], {
+      duration: 2000,
+      iterations: 30,
+    });
   }
-
 };
-flickerDownloadNew()
+flickerDownloadNew();
